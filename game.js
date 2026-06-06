89c2a354677994e207e9262141c537c9b779cb0a
faiq9803922 <faiqahmad3218@gmail.com>
2026-06-06 13:16:33 +0500

    Update game.js

--- game.js
+++ game.js
@@ -18,3 +18,36 @@ requestAnimationFrame(gameLoop);
 }
 
 gameLoop();
+let alpha = 0;
+
+function drawIntro() {
+
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    alpha += 0.02;
+
+    ctx.save();
+
+    ctx.globalAlpha = Math.sin(alpha) * 0.5 + 0.5;
+
+    ctx.font = "120px Arial";
+    ctx.textAlign = "center";
+
+    ctx.shadowColor = "white";
+    ctx.shadowBlur = 40;
+
+    ctx.fillStyle = "white";
+
+    ctx.fillText(
+        "UZELVS",
+        canvas.width / 2,
+        canvas.height / 2
+    );
+
+    ctx.restore();
+
+    if(alpha > 8){
+        scene = "menu";
+    }
+}
